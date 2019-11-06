332d8b1698e211d6712d3dc8ebbd26bd9ca34d45
nitin1438 <nitin.chobhe@gmail.com>
2019-11-06 21:44:42 +0530

    taimen: switch to Ezio's device tree

--- products/taimen/pixeldust_taimen.mk
+++ products/taimen/pixeldust_taimen.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 The PixelDust Project
+# Copyright (C) 2017-2019 The Dirty Unicorns Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -10,7 +10,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-#  limitations under the License.
+# limitations under the License.
 
 # Bootanimation
 BOOTANIMATION := 1440
@@ -19,20 +19,8 @@ BOOTANIMATION := 1440
 PRODUCT_RELEASE_NAME := Pixel2XL
 export TARGET_DEVICE := taimen
 
-# We have our own power HAL
-TARGET_USES_DEVICE_SPECIFIC_POWERHAL := true
-
-# Use the AOSP stack
-TARGET_USES_AOSP := true
-
-# Use the sepolicies which are being shipped with our device
-TARGET_EXCLUDE_QCOM_VENDOR_SEPOLICY := true
-
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
-
-# Generic CAF packages
-#include device/qcom/common/common.mk
 
 # Include common PixelDust stuff
 include vendor/pixeldust/configs/pixeldust_phone.mk
@@ -40,30 +28,31 @@ include vendor/pixeldust/configs/pixeldust_phone.mk
 # Include optional stuff (e.g. prebuilt apps)
 include vendor/pixeldust/configs/system_optional.mk
 
-# Include pixel ambient sense (Now playing)
-#include vendor/pixeldust/configs/ambientsense.mk
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-# Google Apps
-$(call inherit-product, vendor/pixelgapps/pixel-gapps.mk)
-
-# Device identifier. This must come after all inclusions
+# Override AOSP build properties
 PRODUCT_NAME := pixeldust_taimen
-PRODUCT_BRAND := google
+PRODUCT_DEVICE := taimen
+PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel 2 XL
-TARGET_MANUFACTURER := LGE
+PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=taimen \
-    BUILD_FINGERPRINT=google/taimen/taimen:10/QP1A.191105.004/5908170:user/release-keys \
+    PRODUCT_NAME="taimen" \
+    TARGET_DEVICE="taimen" \
     PRIVATE_BUILD_DESC="taimen-user 10 QP1A.191105.004 5908170 release-keys"
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT) \
-    ro.pixeldust.maintainer="nitin1438" \
-    ro.pixeldust.device="taimen"
+BUILD_FINGERPRINT := "google/taimen/taimen:10/QP1A.191105.004/5908170:user/release-keys"
 
-$(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/taimen/taimen:10/QP1A.191105.004/5908170:user/release-keys
 
 # Copy device specific prebuilt files.
 PRODUCT_COPY_FILES += \
     vendor/pixeldust/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+$(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+
