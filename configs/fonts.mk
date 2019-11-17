b781a21dd711585c14dd72eb242258b4f0bf0e05
shagbag913 <sh4gbag913@gmail.com>
2019-11-16 19:10:01 -0500

    DNM: Move font overlays and custom configuration to PixelDust vendor [2/2]
    DO NOT MERGE: will be rebased with proper authorship

--- configs/fonts.mk
+++ configs/fonts.mk
@@ -16,8 +16,25 @@
 PRODUCT_COPY_FILES += \
     vendor/pixeldust/prebuilt/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
+# ThemePicker font overlays
+PRODUCT_PACKAGES += \
+    FontSurferSourceOverlay \
+    FontRubikRubikOverlay \
+    FontLGSmartGothicSourceOverlay \
+    FontArbutusSourceOverlay \
+    FontSonySketchSourceOverlay \
+    FontArvoLatoOverlay \
+    FontAmaranteSourceOverlay \
+    FontAclonicaSourceOverlay \
+    FontCagliostroSourceOverlay \
+    FontCoolstorySourceOverlay \
+    FontRosemarySourceOverlay \
+    FontBariolSourceOverlay
+
 # Fonts
 $(call inherit-product, external/google-fonts/arbutus-slab/fonts.mk)
+$(call inherit-product, external/google-fonts/lato/fonts.mk)
+$(call inherit-product, external/google-fonts/rubik/fonts.mk)
 
 # Prebuilt fonts
 PREBUILT_FONTS := GoogleSans/GoogleSans-Bold GoogleSans/GoogleSans-Medium \
@@ -25,11 +42,15 @@ PREBUILT_FONTS := GoogleSans/GoogleSans-Bold GoogleSans/GoogleSans-Medium \
 	GoogleSans/GoogleSans-Italic GoogleSans/GoogleSans-BoldItalic \
 	ProductSans/ProductSans-LightItalic ProductSans/ProductSans-Regular \
 	ProductSans/ProductSans-Light ProductSans/ProductSans-Thin \
-	ProductSans/ProductSans-ThinItalic ProductSans/ProductSans-Italic GoboldBold-Italic \
-	GoboldThinLight-Italic Gobold-Italic GoboldThinLight GoboldBold Gobold Amarante burnstown \
-	vcr_osd_mono road_rage neoneon dumbledor PhantomBold Cagliostro-Regular Shamshung Surfer \
-	Rosemary-Regular Coolstory-Regular Bariol-Regular SonySketch SamsungOne mexcellent \
-	LGSmartGothic Aclonica snowstorm
+	ProductSans/ProductSans-ThinItalic ProductSans/ProductSans-Italic Gobold/GoboldBold-Italic \
+	Gobold/GoboldThinLight-Italic Gobold/Gobold-Italic Gobold/GoboldThinLight Gobold/GoboldBold \
+	Gobold/Gobold Amarante burnstown road_rage neoneon dumbledor PhantomBold \
+	Cagliostro-Regular Shamshung Surfer Rosemary-Regular Coolstory-Regular Bariol-Regular \
+	SonySketch SamsungOne mexcellent LGSmartGothic Aclonica snowstorm
 
 PRODUCT_COPY_FILES += $(foreach f,$(PREBUILT_FONTS),\
 	vendor/pixeldust/prebuilt/fonts/$(f).ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/$(or $(word 2,$(subst /, ,$(f))),$(f)).ttf)
+
+# Themable font
+PRODUCT_COPY_FILES += \
+    vendor/pixeldust/prebuilt/fonts/vcr_osd_mono.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/ThemeableFont-Regular.ttf
